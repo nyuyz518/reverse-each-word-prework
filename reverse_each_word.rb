3995e76b3eee683f0bbda292434cd68c0b4f1430
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 18:37:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@
 def reverse_each_word (sentence)
   new_array = []
   word_array = sentence.split(" ")
-  word_array.each do |word| 
+  word_array.collect do |word| 
    new_array << word.reverse 
  end 
  new_array.join
